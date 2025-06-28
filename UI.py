e89c9ebfc5d368f241366a5a27a5e4d8e0ebe2f3
DhaviRodrigues <dhavirodrigues2@gmail.com>
2025-06-28 18:43:59 -0300

    adição da UI da tela inicial

---
+++ UI.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+
+# from tkinter import *
+# Explicit imports to satisfy Flake8
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / "TKassets" / "TelaInicial"
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+
+window = Tk()
+
+window.geometry("1280x720")
+window.configure(bg = "#FFFFFF")
+
+
+canvas = Canvas(
+    window,
+    bg = "#FFFFFF",
+    height = 720,
+    width = 1280,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    640.0,
+    360.0,
+    image=image_image_1
+)
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_1 clicked"),
+    relief="flat"
+)
+button_1.place(
+    x=451.80340576171875,
+    y=584.0,
+    width=173.69313049316406,
+    height=72.4165267944336
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_2 clicked"),
+    relief="flat"
+)
+button_2.place(
+    x=641.2868041992188,
+    y=584.0000610351562,
+    width=173.69313049316406,
+    height=72.4165267944336
+)
+
+canvas.create_text(
+    498.0,
+    380.0,
+    anchor="nw",
+    text="BEM – VINDO AO",
+    fill="#44312D",
+    font=("Poppins Black", 32 * -1)
+)
+window.resizable(False, False)
+window.mainloop()
