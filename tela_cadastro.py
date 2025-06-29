9bd00e161433f999bd9ef49016c867c470636bc7
DhaviRodrigues <dhavirodrigues2@gmail.com>
2025-06-29 18:07:20 -0300

    criação da tela de cadastro

--- tela_cadastro.py
+++ tela_cadastro.py
@@ -1,4 +1,4 @@
-from tkinter import Button
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 import tela_inicial
 import tools
 
@@ -9,17 +9,467 @@ def criar_tela_cadastro(window, canvas):
     tools.limpar_tela(canvas)
     canvas.configure(bg="#45312C")
 
-    canvas.create_text(
-        640, 100,
-        text="Tela de Cadastro",
-        fill="#FFFFFF",
-        font=("Poppins Bold", 44 * -1)
-    )
+    canvas.place(x = 0, y = 0)
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    646.0373306274414,
+    365.037353515625,
+    image=image_image_1
+)
 
-    voltar_button = Button(
-        canvas,
-        text="Voltar",
-        font=("Poppins", 16),
-        command=lambda: _transicao_para_inicial(window, canvas)
-    )
-    voltar_button.place(x=50, y=650)
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_1 clicked"),
+    relief="flat"
+)
+button_1.place(
+    x=649.0,
+    y=588.0,
+    width=371.0,
+    height=78.0
+)
+
+canvas.create_text(
+    489.0,
+    132.0,
+    anchor="nw",
+    text="Nome:",
+    fill="#44302C",
+    font=("Poppins Black", 24 * -1)
+)
+
+canvas.create_text(
+    489.0,
+    248.0,
+    anchor="nw",
+    text="Email:",
+    fill="#44302C",
+    font=("Poppins Black", 24 * -1)
+)
+
+canvas.create_text(
+    489.0,
+    364.0,
+    anchor="nw",
+    text="Senha:",
+    fill="#44302C",
+    font=("Poppins Black", 24 * -1)
+)
+
+canvas.create_text(
+    489.0,
+    475.0,
+    anchor="nw",
+    text="Confirme sua senha:",
+    fill="#44302C",
+    font=("Poppins Black", 24 * -1)
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_2 clicked"),
+    relief="flat"
+)
+button_2.place(
+    x=1158.0,
+    y=60.0,
+    width=67.0,
+    height=73.0
+)
+
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+button_3 = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_3 clicked"),
+    relief="flat"
+)
+button_3.place(
+    x=112.0,
+    y=156.0,
+    width=86.0,
+    height=86.0
+)
+
+button_image_4 = PhotoImage(
+    file=relative_to_assets("button_4.png"))
+button_4 = Button(
+    image=button_image_4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_4 clicked"),
+    relief="flat"
+)
+button_4.place(
+    x=219.0,
+    y=156.0,
+    width=88.0,
+    height=86.0
+)
+
+button_image_5 = PhotoImage(
+    file=relative_to_assets("button_5.png"))
+button_5 = Button(
+    image=button_image_5,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_5 clicked"),
+    relief="flat"
+)
+button_5.place(
+    x=327.0,
+    y=156.0,
+    width=86.0,
+    height=86.0
+)
+
+button_image_6 = PhotoImage(
+    file=relative_to_assets("button_6.png"))
+button_6 = Button(
+    image=button_image_6,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_6 clicked"),
+    relief="flat"
+)
+button_6.place(
+    x=112.0,
+    y=279.0,
+    width=86.0,
+    height=86.0
+)
+
+button_image_7 = PhotoImage(
+    file=relative_to_assets("button_7.png"))
+button_7 = Button(
+    image=button_image_7,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_7 clicked"),
+    relief="flat"
+)
+button_7.place(
+    x=219.0,
+    y=279.0,
+    width=87.0,
+    height=86.0
+)
+
+button_image_8 = PhotoImage(
+    file=relative_to_assets("button_8.png"))
+button_8 = Button(
+    image=button_image_8,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_8 clicked"),
+    relief="flat"
+)
+button_8.place(
+    x=327.0,
+    y=279.0,
+    width=86.0,
+    height=86.0
+)
+
+button_image_9 = PhotoImage(
+    file=relative_to_assets("button_9.png"))
+button_9 = Button(
+    image=button_image_9,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_9 clicked"),
+    relief="flat"
+)
+button_9.place(
+    x=112.0,
+    y=409.0,
+    width=86.0,
+    height=86.0
+)
+
+button_image_10 = PhotoImage(
+    file=relative_to_assets("button_10.png"))
+button_10 = Button(
+    image=button_image_10,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_10 clicked"),
+    relief="flat"
+)
+button_10.place(
+    x=219.0,
+    y=409.0,
+    width=87.0,
+    height=86.0
+)
+
+button_image_11 = PhotoImage(
+    file=relative_to_assets("button_11.png"))
+button_11 = Button(
+    image=button_image_11,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_11 clicked"),
+    relief="flat"
+)
+button_11.place(
+    x=327.0,
+    y=409.0,
+    width=86.0,
+    height=86.0
+)
+
+button_image_12 = PhotoImage(
+    file=relative_to_assets("button_12.png"))
+button_12 = Button(
+    image=button_image_12,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_12 clicked"),
+    relief="flat"
+)
+button_12.place(
+    x=112.0,
+    y=537.0,
+    width=86.0,
+    height=86.0
+)
+
+button_image_13 = PhotoImage(
+    file=relative_to_assets("button_13.png"))
+button_13 = Button(
+    image=button_image_13,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_13 clicked"),
+    relief="flat"
+)
+button_13.place(
+    x=219.0,
+    y=537.0,
+    width=87.0,
+    height=86.0
+)
+
+button_image_14 = PhotoImage(
+    file=relative_to_assets("button_14.png"))
+button_14 = Button(
+    image=button_image_14,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_14 clicked"),
+    relief="flat"
+)
+button_14.place(
+    x=327.0,
+    y=537.0,
+    width=86.0,
+    height=86.0
+)
+
+image_image_2 = PhotoImage(
+    file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(
+    261.0,
+    452.0,
+    image=image_image_2
+)
+
+image_image_3 = PhotoImage(
+    file=relative_to_assets("image_3.png"))
+image_3 = canvas.create_image(
+    155.0,
+    452.0,
+    image=image_image_3
+)
+
+image_image_4 = PhotoImage(
+    file=relative_to_assets("image_4.png"))
+image_4 = canvas.create_image(
+    370.0,
+    451.0,
+    image=image_image_4
+)
+
+image_image_5 = PhotoImage(
+    file=relative_to_assets("image_5.png"))
+image_5 = canvas.create_image(
+    262.0,
+    199.0,
+    image=image_image_5
+)
+
+image_image_6 = PhotoImage(
+    file=relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(
+    370.0,
+    322.0,
+    image=image_image_6
+)
+
+image_image_7 = PhotoImage(
+    file=relative_to_assets("image_7.png"))
+image_7 = canvas.create_image(
+    369.0,
+    198.0,
+    image=image_image_7
+)
+
+image_image_8 = PhotoImage(
+    file=relative_to_assets("image_8.png"))
+image_8 = canvas.create_image(
+    155.0,
+    199.0,
+    image=image_image_8
+)
+
+image_image_9 = PhotoImage(
+    file=relative_to_assets("image_9.png"))
+image_9 = canvas.create_image(
+    262.0,
+    322.0,
+    image=image_image_9
+)
+
+image_image_10 = PhotoImage(
+    file=relative_to_assets("image_10.png"))
+image_10 = canvas.create_image(
+    155.0,
+    322.0,
+    image=image_image_10
+)
+
+image_image_11 = PhotoImage(
+    file=relative_to_assets("image_11.png"))
+image_11 = canvas.create_image(
+    261.0,
+    580.0,
+    image=image_image_11
+)
+
+image_image_12 = PhotoImage(
+    file=relative_to_assets("image_12.png"))
+image_12 = canvas.create_image(
+    155.0,
+    580.0,
+    image=image_image_12
+)
+
+image_image_13 = PhotoImage(
+    file=relative_to_assets("image_13.png"))
+image_13 = canvas.create_image(
+    370.0,
+    580.0,
+    image=image_image_13
+)
+
+canvas.create_text(
+    153.0,
+    76.0,
+    anchor="nw",
+    text="Escolha um ícone \n               de perfil ",
+    fill="#EED3B2",
+    font=("Poppins Black", 24 * -1)
+)
+
+canvas.create_text(
+    588.0,
+    70.0,
+    anchor="nw",
+    text="Preencha as informações: ",
+    fill="#44312D",
+    font=("Poppins Black", 36 * -1)
+)
+
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(
+    836.0,
+    200.0,
+    image=entry_image_1
+)
+entry_1 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_1.place(
+    x=489.0,
+    y=171.0,
+    width=694.0,
+    height=56.0
+)
+
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+entry_bg_2 = canvas.create_image(
+    836.0,
+    316.0,
+    image=entry_image_2
+)
+entry_2 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_2.place(
+    x=489.0,
+    y=287.0,
+    width=694.0,
+    height=56.0
+)
+
+entry_image_3 = PhotoImage(
+    file=relative_to_assets("entry_3.png"))
+entry_bg_3 = canvas.create_image(
+    836.0,
+    432.0,
+    image=entry_image_3
+)
+entry_3 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_3.place(
+    x=489.0,
+    y=403.0,
+    width=694.0,
+    height=56.0
+)
+
+entry_image_4 = PhotoImage(
+    file=relative_to_assets("entry_4.png"))
+entry_bg_4 = canvas.create_image(
+    836.0,
+    548.0,
+    image=entry_image_4
+)
+entry_4 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_4.place(
+    x=489.0,
+    y=519.0,
+    width=694.0,
+    height=56.0
+)
+window.resizable(False, False)
+window.mainloop()
